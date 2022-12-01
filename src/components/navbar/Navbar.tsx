5e26e2c825e909fdbec7ee0d7d45e1b979d28f42
GautamSharma05 <gautamsharma88540.com>
2022-12-01 13:31:55 +0530

    🐛 Resolve Navbar Issue

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import "./Navbar.css";
 import searchIcon from "../../assets/images/search-icon.png";
 import hambuger from "../../assets/images/hamburger.png";
 import { useState } from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
   const handleToggle = () => {
@@ -11,38 +11,63 @@ function Navbar() {
   return (
     <nav className="container">
       <div className="logo-section">
-        <Link className="nav-link" to="/">Go
-        <span>ForIt</span>
+        <Link to="/" className="nav-link">
+          Go
+          <span>ForIt</span>
         </Link>
-        
+
         <div className="search-block">
           <input type="text" placeholder="search for rooms or homes..." />
           <img src={searchIcon} alt="search-icon" />
         </div>
       </div>
 
-      <img className="hamburger" onClick={handleToggle} src={hambuger} alt="hamburger" />
+      <img
+        className="hamburger"
+        onClick={handleToggle}
+        src={hambuger}
+        alt="hamburger"
+      />
       <ul className={!showMenu ? "nav-list" : "show"}>
         <li>
-        <Link className="nav-link" to='/'>Home</Link>
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
         </li>
         <li>
-         <Link className="nav-link" to='/about'> About </Link>
+          <Link className="nav-link" to="/about">
+            {" "}
+            About{" "}
+          </Link>
         </li>
         <li>
-          <Link className="nav-link" to='/services'> Services</Link>
+          <Link className="nav-link" to="/services">
+            {" "}
+            Services
+          </Link>
         </li>
         <li>
-          <Link className="nav-link" to="/appartment-room"> Apartment Room</Link>
+          <Link className="nav-link" to="/appartment-room">
+            {" "}
+            Apartment Room
+          </Link>
         </li>
         <li>
-          <Link className="nav-link" to="/blog">Blog</Link>
+          <Link className="nav-link" to="/blog">
+            Blog
+          </Link>
         </li>
         <li>
-          <Link className="nav-link" to="/contact"> Contact</Link>
+          <Link className="nav-link" to="/contact">
+            {" "}
+            Contact
+          </Link>
         </li>
         <li>
-          <Link className="nav-link" to="/login"> Login/SignUp</Link>
+          <Link className="nav-link" to="/login">
+            {" "}
+            Login/SignUp
+          </Link>
         </li>
       </ul>
     </nav>
