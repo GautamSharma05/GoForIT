879c8c785b09811d529528fa685b725fcd1d6382
GautamSharma05 <gautamsharma88540.com>
2022-12-01 18:16:53 +0530

    🐛 Fix Production Url Issue

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
       <Header />
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/GoForIT" element={<Home/>} />
         <Route path="/about" element={<About />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
